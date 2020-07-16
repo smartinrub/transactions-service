d03965502d7551e010e865a6963293caaaf6cf4c
Sergio Martin Rubio <econsergio@gmail.com>
2020-07-16 23:51:52 +0100

    Add another constructor to exception

--- src/main/java/com/sergiomartinrubio/transactionsservice/exception/TransactionNotFoundException.java
+++ src/main/java/com/sergiomartinrubio/transactionsservice/exception/TransactionNotFoundException.java
@@ -3,12 +3,19 @@ package com.sergiomartinrubio.transactionsservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.UUID;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TransactionNotFoundException extends RuntimeException {
 
     private static final String MESSAGE = "Transaction not found for account iban %s";
+    private static final String MESSAGE_REFERENCE = "Transaction not found for reference %s";
 
     public TransactionNotFoundException(String accountIban) {
         super(String.format(MESSAGE, accountIban));
+    }
+
+    public TransactionNotFoundException(UUID reference) {
+        super(String.format(MESSAGE_REFERENCE, reference));
     }
 }
