d243aa0690f13d7ef3b2afd87b468be0bf7c0f99
Sergio Martin Rubio <econsergio@gmail.com>
2020-07-16 23:52:05 +0100

    Replace string with uuid

--- src/main/java/com/sergiomartinrubio/transactionsservice/repository/TransactionRepository.java
+++ src/main/java/com/sergiomartinrubio/transactionsservice/repository/TransactionRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
     @Query("SELECT t FROM Transaction t WHERE account_iban = ?1")
     Optional<Transaction> searchTransaction(String accountIban);
