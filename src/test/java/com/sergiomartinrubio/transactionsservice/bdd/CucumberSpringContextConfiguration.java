5097c5c61948c672407553b80b86bd0b09feba48
Sergio Martin Rubio <econsergio@gmail.com>
2020-08-15 15:58:29 +0100

    Clean up

--- src/test/java/com/sergiomartinrubio/transactionsservice/bdd/CucumberSpringContextConfiguration.java
+++ src/test/java/com/sergiomartinrubio/transactionsservice/bdd/CucumberSpringContextConfiguration.java
@@ -4,9 +4,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringContextConfiguration {
